3b1dec66dfd4a6fa61855eb8a41fbd9bd87858af
Joan R. Bellido <j.bellido@hotelbeds.com>
2020-03-26 09:10:48 +0100

    Format code

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 module.exports = {
-  parallel: (tasks, max, delay, clbk) => {
-    var taskQueue = tasks.reverse();
-    var allFinished = false;
-    var current = 0;
+    parallel: (tasks, max, delay, clbk) => {
+        var taskQueue = tasks.reverse();
+        var allFinished = false;
+        var current = 0;
 
-    (function resolveTasks() {
-      if (taskQueue.length > 0) {
-        if (current < max) {
-          for (var i = 0; i < max - current; i++) {
-            current++;
-            taskQueue.pop()(
-              function resolveFn() {
-                current--;
-              },
-              function rejectFn() {
-                current--;
-              });
-              setTimeout(resolveTasks, delay);
-          }
-        } else {
-          setTimeout(resolveTasks, delay);
-        }
-      } else {
-        if (current === 0 && !allFinished) {
-          allFinished = true;
-          if (typeof clbk === 'function') {
-            clbk();
-          }
-        } else {
-          setTimeout(resolveTasks, delay);
-        }
-      }
-    })();
-  }
+        (function resolveTasks() {
+            if (taskQueue.length > 0) {
+                if (current < max) {
+                    for (var i = 0; i < max - current; i++) {
+                        current++;
+                        taskQueue.pop()(
+                            function resolveFn() {
+                                current--;
+                            },
+                            function rejectFn() {
+                                current--;
+                            });
+                        setTimeout(resolveTasks, delay);
+                    }
+                } else {
+                    setTimeout(resolveTasks, delay);
+                }
+            } else {
+                if (current === 0 && !allFinished) {
+                    allFinished = true;
+                    if (typeof clbk === 'function') {
+                        clbk();
+                    }
+                } else {
+                    setTimeout(resolveTasks, delay);
+                }
+            }
+        })();
+    }
 }
-
