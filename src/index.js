4ddd235a98ddbe93a05e309ed7aff080d40e7ac3
Joan R. Bellido <j.bellido@hotelbeds.com>
2020-03-26 09:50:17 +0100

    Fix issue with queue

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 module.exports = {
-    parallel: (tasks, max, delay, clbk) => {
+    parallel: (tasks, maxConcurrency, delay, callback) => {
         var taskQueue = tasks.reverse();
         var allFinished = false;
         var current = 0;
 
         (function resolveTasks() {
+            if (allFinished) {
+                return;
+            }
             if (taskQueue.length > 0) {
-                if (current < max) {
-                    for (var i = 0; i < max - current; i++) {
-                        current++;
-                        taskQueue.pop()(
-                            function resolveFn() {
-                                current--;
-                            },
-                            function rejectFn() {
-                                current--;
-                            });
-                        setTimeout(resolveTasks, delay);
+                if (current < maxConcurrency) {
+                    var toTake = Math.min(maxConcurrency - current, taskQueue.length);
+                    for (var i = 0; i < toTake; i++) {
+                        var task = taskQueue.pop();
+                        if (typeof task === 'function') {
+                            current++;
+                            task.apply(this, [
+                                function resolveFn() {
+                                    current--;
+                                },
+                                function rejectFn() {
+                                    current--;
+                                }
+                            ]);
+                            setTimeout(resolveTasks, delay);
+                        }
                     }
                 } else {
                     setTimeout(resolveTasks, delay);
@@ -24,8 +32,8 @@ module.exports = {
             } else {
                 if (current === 0 && !allFinished) {
                     allFinished = true;
-                    if (typeof clbk === 'function') {
-                        clbk();
+                    if (typeof callback === 'function') {
+                        callback();
                     }
                 } else {
                     setTimeout(resolveTasks, delay);
--- test/index.js
+++ test/index.js
@@ -7,45 +7,44 @@ jobs.push(function(resolve, reject) {
         var data = "job1 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 jobs.push(function(resolve, reject) {
     setTimeout(function() {
         var data = "job2 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 jobs.push(function(resolve, reject) {
     setTimeout(function() {
         var data = "job3 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 jobs.push(function(resolve, reject) {
     setTimeout(function() {
         var data = "job4 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 jobs.push(function(resolve, reject) {
     setTimeout(function() {
         var data = "job5 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 jobs.push(function(resolve, reject) {
     setTimeout(function() {
         var data = "job6 - done!";
         console.log(data);
         resolve(data);
-    }, 2500);
+    }, 500);
 });
 
-asyncThrottle.parallel(jobs, 2, 200, () => {
+asyncThrottle.parallel(jobs, 2, 100, () => {
   console.log('all done.');
 });
-
