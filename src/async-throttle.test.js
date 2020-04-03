00da13f0afc3575a9b067abf300c54cb97408ae4
Joan R <jrbellido@gmail.com>
2020-04-03 23:38:57 +0200

    Improve tests

--- src/async-throttle.test.js
+++ src/async-throttle.test.js
@@ -19,31 +19,31 @@ function createJobs(count, time, output) {
 test('works for concurrency of 1', async () => {
   let result = [];
   let jobs = createJobs(6, 100, result);
-  await asyncThrottle.parallel(jobs, 1, 10);
+  await asyncThrottle.parallel(jobs, 1, 50);
 
-  expect( result[1] - result[0]).toBeGreaterThanOrEqual(100);
-  expect( result[2] - result[1]).toBeGreaterThanOrEqual(100);
-  expect( result[3] - result[2]).toBeGreaterThanOrEqual(100);
-  expect( result[4] - result[3]).toBeGreaterThanOrEqual(100);
+  expect(result[1] - result[0]).toBeGreaterThanOrEqual(100);
+  expect(result[2] - result[1]).toBeGreaterThanOrEqual(100);
+  expect(result[3] - result[2]).toBeGreaterThanOrEqual(100);
+  expect(result[4] - result[3]).toBeGreaterThanOrEqual(100);
 });
 
 test('works for concurrency of 2', async () => {
   let result = [];
   let jobs = createJobs(6, 100, result);
-  await asyncThrottle.parallel(jobs, 2, 10);
+  await asyncThrottle.parallel(jobs, 2, 50);
 
-  expect(result[0]).toBe(result[1]);
-  expect(result[2]).not.toBe(result[1]);
-  expect(result[3]).toBe(result[2]);
+  expect(result[1] - result[0]).toBeLessThanOrEqual(50);
+  expect(result[2] - result[1]).not.toBeLessThanOrEqual(50);
+  expect(result[3] - result[2]).toBeLessThanOrEqual(50);
 });
 
 test('works for concurrency of 3', async () => {
   let result = [];
   let jobs = createJobs(6, 100, result);
-  await asyncThrottle.parallel(jobs, 3, 10);
-
-  expect(result[0]).toBe(result[2]);
-  expect(result[0]).not.toBe(result[3]);
-  expect(result[3]).toBe(result[4]);
+  await asyncThrottle.parallel(jobs, 3, 50);
+  
+  expect(result[2] - result[0]).toBeLessThanOrEqual(50);
+  expect(result[3] - result[0]).not.toBeLessThanOrEqual(50);
+  expect(result[4] - result[3]).toBeLessThanOrEqual(50);
 });
 
